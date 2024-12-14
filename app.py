ef1425b4349e8663da56ff85a361ea1dfef6ae3b
MyGitHubLEA <168448851+MyGitHubLEA@users.noreply.github.com>
2024-12-15 01:22:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from flask import Flask, jsonify, request, send_file, render_template, send_from_directory, redirect, url_for, flash
-#from flask_toastr import Toastr
 from collections import deque
 import time
 import os
 import psutil
 import requests
 import socket
+from minio import Minio
+from dotenv import load_dotenv
+from urllib.request import urlopen
+
+load_dotenv()
+
+LOCAL_FILE_PATH = os.environ.get('LOCAL_FILE_PATH')
+ACCESS_KEY = os.environ.get('ACCESS_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+MINIO_API_HOST = "http://localhost:9000"
+
+MINIO_CLIENT = Minio("localhost:9000", access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False)
 
 app = Flask(__name__)
 
+if not os.path.exists("files"):
+    os.makedirs("files")
     
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -34,35 +47,10 @@ def head():
 @app.route("/", methods=["GET"])
 def index():
     #filenames = os.listdir(app.config['UPLOAD_FOLDER'])
-    filenames = os.listdir(app.config['UPLOAD_FOLDER'])
+    objects = MINIO_CLIENT.list_objects('mybucket', prefix=None, recursive = True)
+    filenames = [object.object_name for object in objects]
+    get_bucket_files()
     return render_template('index.html', filenames=filenames), 200, {"Content-Type": "text/html"}
-
-
-'''@app.route("/favicon.ico")
-def favicon():
-    return app.send_static_file("favicon.ico")
-'''
-
-@app.route("/<filename>")
-def get_file(filename):
-    if os.path.isfile(os.path.join("files", filename)):
-        return (
-            open(os.path.join("files", filename), "r").read(),
-            200,
-            {"Content-Type": "application/json"},
-        )
-    for node in nodes:
-        try:
-            response = requests.get(f"{node}/{filename}", params={"key": password})
-            if response.status_code == 200:
-                return response.content, 200, {"Content-Type": "application/json"}
-        except:
-            continue
-    return (
-        jsonify({"error": "File not found"}),
-        404,
-        {"Content-Type": "application/json"},
-    )
 
 
 @app.route("/upload", methods=["POST"])
@@ -74,13 +62,32 @@ def load_file():
         return "File was not chosen"
     if file:
         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        path = os.path.join(request.base_url, file.filename)           
         file.save(filename)
+        MINIO_CLIENT.fput_object("mybucket", file.filename, filename, file.mimetype)
+        os.remove(filename)
         return redirect('/')
+ 
+
+def get_bucket_files():
+        #try:
+        objects = MINIO_CLIENT.list_objects('mybucket', prefix=None, recursive = True) # prefix The object name prefix for filtering
+        for obj in objects:
+            print(obj.bucket_name, obj.object_name.encode('utf-8'), obj.last_modified,
+                  obj.etag, obj.size, obj.content_type)
+        #except ResponseError as err:
+        #    print(err)
+
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    res = MINIO_CLIENT.fget_object("mybucket", filename, filename)
+    url = MINIO_CLIENT.get_presigned_url(
+    "GET",
+    "mybucket",
+    filename,
+    )
+    opened_url =  urlopen(url)
+    return send_file(opened_url, mimetype=res.content_type)
 
 
 @app.route("/status", methods=["GET"])
@@ -95,6 +102,14 @@ def status():
     free_space = int(usage.free / 1024)
     return jsonify({"free_space": free_space})
 
+@app.route('/delete/<filename>', methods=['POST', 'GET'])
+def delete_file(filename):
+    #try:
+    MINIO_CLIENT.remove_object('mybucket', filename)
+    print('Object removed successfully')
+    return redirect(url_for('index'))
+    #except ResponseError as err:
+    #    print(err)
 '''
 @app.route("/config", methods=["GET"])
 def config():
